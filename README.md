6b9cf8e51059f4b33cc51675392220dca5cf7f8e
Esteban Fuentealba <efuentealba@json.cl>
2017-03-05 20:18:47 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # node-flow-cl
 NodeJS package para la integración de pagos con Flow (https://www.flow.cl)
+
+# usage
+
+```javascript
+const Flow = require("node-flow-cl");
+var flowAPI = new Flow({
+    "FLOW_URL_EXITO": "http://www.comercio.cl/kpf/exito.php",
+    "FLOW_URL_FRACASO": "http://www.comercio.cl/kpf/fracaso.php",
+    "FLOW_URL_CONFIRMACION": "http://www.comercio.cl/kpf/confirma.php",
+    "FLOW_URL_RETORNO": "http://www.comercio.cl",
+    "FLOW_URL_PAGO": "http://flow.tuxpan.com/app/kpf/pago.php",
+    "FLOW_KEY": "/keys",
+    "FLOW_LOGPATH": "/logs",
+    "FLOW_COMERCIO": "emailFlow@comercio.com",
+    "FLOW_MEDIOPAGO": "9",
+    "FLOW_TIPO_INTEGRACION": "f"
+});
+var flow_pack = flowAPI.new_order("123", "4500", "", "efuentealba@json.cl");
+
+//  use flow_pack in form post
+```
---
+++ kpf/flowApi.js
@@ -0,0 +1,65 @@
+const htmlentities = require('locutus/php/strings/htmlentities');
+const crypto = require("crypto");
+const fs = require("fs");
+
+class flowAPI {
+    constructor(options) {
+        let defaults = {
+            "FLOW_URL_EXITO": "http://www.comercio.cl/kpf/exito.php",
+            "FLOW_URL_FRACASO": "http://www.comercio.cl/kpf/fracaso.php",
+            "FLOW_URL_CONFIRMACION": "http://www.comercio.cl/kpf/confirma.php",
+            "FLOW_URL_RETORNO": "http://www.comercio.cl",
+            "FLOW_URL_PAGO": "http://flow.tuxpan.com/app/kpf/pago.php",
+            "FLOW_KEY": "/keys",
+            "FLOW_LOGPATH": "/logs",
+            "FLOW_COMERCIO": "emailFlow@comercio.com",
+            "FLOW_MEDIOPAGO": "9",
+            "FLOW_TIPO_INTEGRACION": "f"
+        };
+        this.config = Object.assign({}, defaults, options );
+    }
+    flow_pack() {
+		var tipo_integracion = encodeURIComponent(config.FLOW_TIPO_INTEGRACION);
+		var comercio = encodeURIComponent(config.FLOW_COMERCIO);
+		var orden_compra = encodeURIComponent(this.order.OrdenNumero);
+		var monto = encodeURIComponent(this.order.Monto);
+		var medioPago = encodeURIComponent(this.order.MedioPago);
+		var email = encodeURIComponent(this.order.Pagador);
+
+        var hConcepto = htmlentities(this.order.Concepto);
+        if (!hConcepto) hConcepto = htmlentities(this.order.Concepto, 2, 'UTF-8');
+        if (!hConcepto) hConcepto = htmlentities(this.order.Concepto, 2, 'ISO-8859-1');
+        if (!hConcepto) hConcepto = `Orden de Compra ${orden_compra}`;
+		var concepto = encodeURIComponent(hConcepto);
+		
+		var url_exito = encodeURIComponent(config.FLOW_URL_EXITO);
+		var url_fracaso = encodeURIComponent(config.FLOW_URL_FRACASO);
+		var url_confirmacion = encodeURIComponent(config.FLOW_URL_CONFIRMACION);
+		var url_retorno = encodeURIComponent(config.FLOW_URL_RETORNO);
+
+
+        var p = `c=${comercio}&oc=${orden_compra}&mp=${medioPago}&m=${monto}&o=${concepto}&ue=${url_exito}&uf=${url_fracaso}&uc=${url_confirmacion}&ti=${tipo_integracion}&e=${email}&v=kit_1_4&ur=${url_retorno}`;
+        
+		var signature = this.flow_sign(p);
+		return p + `&s=${signature}`;
+
+    }
+    flow_sign(data) {
+        var privateKey = fs.readFileSync(`${config.FLOW_KEY}`,"utf8");
+        var signature = encodeURIComponent(crypto.createSign('sha256').update(data).sign(privateKey,"base64"));   
+		return signature;
+	}
+    new_order(orden_compra, monto,  concepto, email_pagador, medioPago = "Non") {
+		if(medioPago == "Non") {
+			medioPago = config.FLOW_MEDIOPAGO;
+		}
+		this.order.OrdenNumero = orden_compra;
+		this.order.Concepto = concepto;
+		this.order.Monto = monto;
+		this.order.MedioPago = medioPago;
+		this.order.Pagador = email_pagador;
+		return this.flow_pack();
+	}
+
+}
+module.exports = flowAPI;
